20d5b47e98671691493ad52b9e38d97ecd64f6b0
SonOfSatan <Morten1990@hotmail.com>
2014-11-28 10:22:45 +0100

    BLL Layer Fix

---
+++ BLLTier/BLL_API/Models/OrderLineDTO.cs
@@ -0,0 +1,16 @@
+﻿
+namespace DAL.DTOModels
+{
+    public class OrderLineDTO: GenericDTO
+    {
+        public int OrderId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Amount { get; set; }
+
+        public decimal LineTotal { get; set; }
+
+        public int id { get; set; }
+    }
+}
