87afa921a98583fd31fe28c8e39285a4997c3689
SpamHam <peterthraneolsen@gmail.com>
2014-11-30 23:09:11 +0100

    Products bll added

---
+++ BLLTier/TestBLL_API/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestBLL_API
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
