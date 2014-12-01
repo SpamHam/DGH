29de2a641171d0faec050d787af8284d0acd3d7b
jacobfullbuster <jacob-abildskov@hotmail.com>
2014-12-01 12:11:46 +0100

    Category DAL

---
+++ DALTier/DAL/Entities Converter/CategoryConverter.cs
@@ -0,0 +1,32 @@
+﻿using DAL.DTOModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities_Converter
+{
+    class CategoryConverter
+    {
+        public static Category ToCategory(CategoryDTO categoryDTO)
+        {
+            var category = new Category()
+            {
+               id = categoryDTO.id,
+               categoryName = categoryDTO.categoryName
+            };
+            return category;
+        }
+        public static CategoryDTO ToCategoryDTO(Category category)
+        {
+            var categoryDTO = new CategoryDTO()
+            {
+                id = category.id,
+                categoryName = category.categoryName
+            };
+
+            return categoryDTO;
+        }
+    }
+}
---
+++ DALTier/DAL/Repository/Impl/CategoryRepository.cs
@@ -0,0 +1,44 @@
+﻿using DAL.DTOModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities_Converter;
+using System.Data.Entity;
+
+namespace DAL.Repository.Impl
+{
+    internal class CategoryRepository: IGenericRepository<CategoryDTO>
+    {
+
+        public CategoryDTO Get(DGHEntities db, int id)
+        {
+            return db.Categories.Select(CategoryConverter.ToCategoryDTO).FirstOrDefault(x => x.id == id);
+        }
+
+        public IEnumerable<CategoryDTO> GetAll(DGHEntities db)
+        {
+            return db.Categories.Select(CategoryConverter.ToCategoryDTO).ToList();
+        }
+
+        public void Create(CategoryDTO categoryDTO, DGHEntities db)
+        {
+            if (categoryDTO == null) throw new ArgumentNullException("categoryDTO");
+            db.Categories.Add(CategoryConverter.ToCategory(categoryDTO));
+            db.SaveChanges();
+        }
+
+        public void Update(CategoryDTO categoryDTO, DGHEntities db)
+        {
+            if (categoryDTO == null) throw new ArgumentNullException("categoryDTO");
+            db.Entry(CategoryConverter.ToCategory(categoryDTO)).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public void Delete(int id, DGHEntities db)
+        {
+            
+        }
+    }
+}
