012439e525f5ab6351b370a4a141b9d16eb0d831
jacobfullbuster <jacob-abildskov@hotmail.com>
2014-12-02 12:16:12 +0100

    Ændring i dal category

---
+++ DALTier/DAL_API/Controllers/CategoryController.cs
@@ -0,0 +1,120 @@
+﻿using DAL;
+using DAL.DTOModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DAL_API.Controllers
+{
+    [RoutePrefix("category")]
+    public class CategoryController : ApiController
+    {
+        private readonly Facade _facade;
+
+        private CategoryController()
+        {
+            _facade = new Facade();
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<CategoryDTO> GetAll()
+        {
+            return _facade.GetCategoryRepository().GetAll();
+        }
+
+        /// <summary>
+        /// Will get a specific Genre found by the Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{id:int}", Name = "GetCategoryById")]
+        public HttpResponseMessage Get(int id)
+        {
+            var category = _facade.GetCategoryRepository().Get(id);
+            if (category != null)
+            {
+                return Request.CreateResponse<CategoryDTO>(HttpStatusCode.OK, category);
+            }
+            var response = new HttpResponseMessage(HttpStatusCode.NotFound)
+            {
+                Content = new StringContent("Product not found.")
+            };
+            throw new HttpResponseException(response);
+        }
+
+        /// <summary>
+        /// Creates a Genre in the Database
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("")]
+        public HttpResponseMessage Post(CategoryDTO category)
+        {
+            try
+            {
+                _facade.GetCategoryRepository().Create(category);
+
+                var response = Request.CreateResponse<CategoryDTO>(HttpStatusCode.Created, category);
+                var uri = Url.Link("GetCategoryById", new { category.id });
+                response.Headers.Location = new Uri(uri);
+                return response;
+            }
+            catch (Exception)
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.Conflict)
+                {
+                    Content = new StringContent("Could not add category to db")
+                };
+                throw new HttpResponseException(response);
+            }
+        }
+
+        /// <summary>
+        /// Updates a Genre in Database
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("")]
+        public HttpResponseMessage Put(CategoryDTO category)
+        {
+            try
+            {
+                _facade.GetCategoryRepository().Update(category);
+                var response = Request.CreateResponse<CategoryDTO>(HttpStatusCode.OK, category);
+                var uri = Url.Link("GetProductById", new { category.id });
+                response.Headers.Location = new Uri(uri);
+                return response;
+            }
+            catch (Exception)
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.Conflict)
+                {
+                    Content = new StringContent("No matching category")
+                };
+                throw new HttpResponseException(response);
+            }
+        }
+
+        /// <summary>
+        /// Delete a Genre
+        /// </summary>
+        /// <param name="id"></param>
+        [HttpDelete]
+        [Route("{id:int}")]
+        public HttpResponseMessage Delete(int id)
+        {
+
+            _facade.GetCategoryRepository().Delete(id);
+            var response = new HttpResponseMessage(HttpStatusCode.Accepted);
+            return response;
+
+        }
+    }
+}
