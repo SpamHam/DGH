58de9e266bd148a3109b2394b3601690e96bbe17
schQneberg <clausschoneberg88@gmail.com>
2014-12-03 10:56:39 +0100

    Euro Converter added

---
+++ BLLTier/BLL/Logic/CurrencyConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BLL.Logic
+{
+    class CurrencyConverter
+    {
+        private decimal EuroInDKK;
+        string path = "http://www.nationalbanken.dk/_vti_bin/DN/DataService.svc/CurrencyRatesXML?lang=da";
+
+        public decimal EuroToDKK(){
+            
+            XmlDocument theDocument = new XmlDocument();
+            theDocument.Load(path);
+            XmlNode node = theDocument.SelectSingleNode("//currency[@code='EUR']");  
+        return EuroInDKK = Convert.ToDecimal(node.Attributes["rate"].Value) / 100;}
+    }       
+    }
