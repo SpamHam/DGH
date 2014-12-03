d58259ce1fc1951bf71f1f7eaef1f4ff55d49717
SpamHam <peterthraneolsen@gmail.com>
2014-12-03 11:17:22 +0100

    produ

---
+++ MVCAdminTier/MVC_DGHAdmin/Controllers/ProductController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BLLGateway.DTOModels;
+using MVC_DGHAdmin.Models;
+
+namespace MVC_DGHAdmin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(db.ProductDTOes.ToList());
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductDTO productDTO = db.ProductDTOes.Find(id);
+            if (productDTO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productDTO);
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,name,productNumber,color,stock,salesPrice,categoryId,imageId")] ProductDTO productDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProductDTOes.Add(productDTO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(productDTO);
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductDTO productDTO = db.ProductDTOes.Find(id);
+            if (productDTO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productDTO);
+        }
+
+        // POST: Product/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name,productNumber,color,stock,salesPrice,categoryId,imageId")] ProductDTO productDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productDTO).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(productDTO);
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductDTO productDTO = db.ProductDTOes.Find(id);
+            if (productDTO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productDTO);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProductDTO productDTO = db.ProductDTOes.Find(id);
+            db.ProductDTOes.Remove(productDTO);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
