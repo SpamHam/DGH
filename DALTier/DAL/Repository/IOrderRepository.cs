0848addd79d85427388d8cad1a74fec7cd927bc6
SonOfSatan <Morten1990@hotmail.com>
2014-12-04 13:36:25 +0100

    IOrderRepository

---
+++ DALTier/DAL/Repository/IOrderRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using DAL.DTOModels;
+
+namespace DAL.Repository
+{
+        public interface IOrderRepository
+        {
+            OrderDTO Get(int id);
+
+            IEnumerable<OrderDTO> GetAll();
+
+            void Add(OrderDTO orderDTO);
+
+            void Update(OrderDTO orderDTO);
+
+            void Delete(int id);
+
+            IEnumerable<OrderModelDTO> GetViewModel();
+        }
+}
