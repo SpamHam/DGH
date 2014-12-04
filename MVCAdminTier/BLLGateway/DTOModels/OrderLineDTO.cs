8534f5841956787079dcbf6bb0ff59d22cc04911
SonOfSatan <Morten1990@hotmail.com>
2014-12-04 13:35:59 +0100

    Fix

--- MVCAdminTier/BLLGateway/DTOModels/OrderLineDTO.cs
+++ MVCAdminTier/BLLGateway/DTOModels/OrderLineDTO.cs
@@ -1,7 +1,8 @@
 ﻿
 namespace BLLGateway.DTOModels
 {
-    public class OrderLineDTO: IGenericDTO
+    //CRUD functionallity.
+    public class OrderLineDTO : IGenericDTO
     {
         public int OrderId { get; set; }
 
@@ -12,5 +13,21 @@ namespace BLLGateway.DTOModels
         public decimal LineTotal { get; set; }
 
         public int id { get; set; }
+    }
+
+    //For presentation view.
+    public class OrderLineModelDTO
+    {
+        public int id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ProductPicture { get; set; }
+
+        public decimal ProductPrice { get; set; }
+
+        public int Amount { get; set; }
+
+        public decimal LineTotal { get; set; }
     }
 }
