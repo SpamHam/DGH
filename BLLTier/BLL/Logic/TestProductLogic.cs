f63f56c8de0403bf7aa5f622a01015fef865e32d
SpamHam <peterthraneolsen@gmail.com>
2014-12-07 16:41:22 +0100

    change to unit due to ci problems

--- BLLTier/BLL/Logic/TestProductLogic.cs
+++ BLLTier/BLL/Logic/TestProductLogic.cs
@@ -1,5 +1,4 @@
 ﻿using BLL.DTOModels;
-using BLL.Logic;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -7,10 +6,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test_logic
+namespace BLL.Logic
 {
     [TestFixture]
-    public class ProductLogicTest
+   public class TestProductLogic
     {
         [SetUp]
         public void SetUp()
@@ -26,21 +25,21 @@ namespace Test_logic
         [Test]
         public void getActiveProductTest()
         {
-           var p = ProductSorter.getActiveProducts(GetTestProducts());
-           Assert.AreEqual(p.Count(), 2);
-           foreach (var f in p)
-           {
-               Assert.IsTrue(f.active);
-           }
+            var p = ProductSorter.getActiveProducts(GetTestProducts());
+            Assert.AreEqual(p.Count(), 2);
+            foreach (var f in p)
+            {
+                Assert.IsTrue(f.active);
+            }
         }
 
         private List<ProductDTO> GetTestProducts()
         {
             var testProducts = new List<ProductDTO>();
-            testProducts.Add(new ProductDTO { id = 1, name = "Demo1", productNumber = "A1", active = true, categoryId = 1, color = "Blue", imageUrl ="", salesPrice = 120, stock = 40 });
+            testProducts.Add(new ProductDTO { id = 1, name = "Demo1", productNumber = "A1", active = true, categoryId = 1, color = "Blue", imageUrl = "", salesPrice = 120, stock = 40 });
             testProducts.Add(new ProductDTO { id = 2, name = "Demo2", productNumber = "A2", active = false, categoryId = 2, color = "Orange", imageUrl = "", salesPrice = 220, stock = 20 });
             testProducts.Add(new ProductDTO { id = 3, name = "Demo3", productNumber = "A3", active = true, categoryId = 3, color = "Yellow", imageUrl = "", salesPrice = 320, stock = 30 });
-            testProducts.Add(new ProductDTO { id = 4, name = "Demo4", productNumber = "A4", active = false, categoryId = 4 , color = "Black", imageUrl = "", salesPrice = 420, stock = 10 });
+            testProducts.Add(new ProductDTO { id = 4, name = "Demo4", productNumber = "A4", active = false, categoryId = 4, color = "Black", imageUrl = "", salesPrice = 420, stock = 10 });
 
             return testProducts;
         }
