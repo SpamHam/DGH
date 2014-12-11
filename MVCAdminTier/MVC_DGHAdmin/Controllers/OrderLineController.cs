2ac9a3853551f4e7c635760629e1e5e1a184e96f
SonOfSatan <Morten1990@hotmail.com>
2014-12-11 08:49:57 +0100

    I AM AWESOME

--- MVCAdminTier/MVC_DGHAdmin/Controllers/OrderLineController.cs
+++ MVCAdminTier/MVC_DGHAdmin/Controllers/OrderLineController.cs
@@ -1,33 +1,36 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
 using System.Web.Mvc;
 using BLLGateway.DTOModels;
 using BLLGateway.Gateway;
+using BLLGateway.Gateway.Gateways;
+using MVC_DGHAdmin.Models;
 
 namespace MVC_DGHAdmin.Controllers
 {
     public class OrderLineController : Controller
     {
         private readonly IGenericGateway<OrderLineDTO> _orderLineGateway = new Facade().GetOrderLineGateway();
+        private readonly IGenericGateway<ProductDTO> _productGateway = new Facade().GetProductGateway(); 
         private readonly String _url = "orderline";
 
-        public ActionResult Details(int? id)
+        public ActionResult Create(int? orderId)
         {
-            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            var orderLineDTO = _orderLineGateway.Get(_url, (int) id);
-            if (orderLineDTO != null) return View(orderLineDTO);
-            return HttpNotFound();
-        }
-
-        public ActionResult Create()
-        {
-            return View();
+            if (orderId == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            return View(new OrderLineViewModels
+            {
+                DropProduct = new SelectList(_productGateway.GetAll("product").ToList(), "id", "name"),
+                OrderLine = new OrderLineDTO {OrderId = (int) orderId},
+                Product = _productGateway.GetAll("product")
+            });
         }
         
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "id,OrderId,ProductId,Amount,LineTotal")] OrderLineDTO orderLineDTO)
+        public ActionResult Create([Bind(Include = "id,OrderId,ProductId,Amount")] OrderLineDTO orderLineDTO)
         {
+
             if (!ModelState.IsValid) return View(orderLineDTO);
             _orderLineGateway.Add(orderLineDTO, _url);
             return RedirectToAction("Index", "OrderController");
@@ -36,14 +39,17 @@ namespace MVC_DGHAdmin.Controllers
         public ActionResult Edit(int? id)
         {
             if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            var orderLineDTO = _orderLineGateway.Get(_url, (int) id);
-            if (orderLineDTO != null) return View(orderLineDTO);
-            return HttpNotFound();
+            return View(new OrderLineViewModels
+            {
+                DropProduct = new SelectList(_productGateway.GetAll("product").ToList(), "id", "name"),
+                OrderLine = _orderLineGateway.Get(_url, (int) id),
+                Product = _productGateway.GetAll("product")
+            });
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "id,OrderId,ProductId,Amount,LineTotal")] OrderLineDTO orderLineDTO)
+        public ActionResult Edit([Bind(Include = "id,OrderId,ProductId,Amount")] OrderLineDTO orderLineDTO)
         {
             if (!ModelState.IsValid) return View(orderLineDTO);
             _orderLineGateway.Update(orderLineDTO, _url);
