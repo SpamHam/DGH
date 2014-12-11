f577907d2f6c1c2adee46d5223ed3b947729d936
jacobfullbuster <jacob-abildskov@hotmail.com>
2014-12-11 13:37:05 +0100

    Bug fix på address controller

---
+++ BLLTier/BLL/GateWay/ICityGateway.cs
@@ -0,0 +1,15 @@
+﻿using BLL.DTOModels;
+using BLL.Gateway;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.GateWay
+{
+    public interface ICityGateway : IGenericGateway<CityDTO>
+    {
+        CityDTO getCityByZipcode(String path, String zipcode);
+    }
+}
