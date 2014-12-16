5e46e119f9671b6b70f34d656733512dea8addb3
SpamHam <peterthraneolsen@gmail.com>
2014-12-16 23:36:45 +0100

    products view cleaned

--- MVCAdminTier/BLLGateway/DTOModels/ProductDTO.cs
+++ MVCAdminTier/BLLGateway/DTOModels/ProductDTO.cs
@@ -4,12 +4,17 @@ namespace BLLGateway.DTOModels
  public class ProductDTO: IGenericDTO
     {
         public int id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
         public string name { get; set; }
         [Display(Name = "product number")]
+        [Required(ErrorMessage = "product number is required")]
         public string productNumber { get; set; }
+     [Required(ErrorMessage = "Color is required")]
         public string color { get; set; }
+     [Required(ErrorMessage = "stock number is required")]
         public int stock { get; set; }
      [Display(Name = "sales price")]
+     [Required(ErrorMessage = "Sales price is required")]
         public decimal salesPrice { get; set; }
         public int categoryId { get; set; }
      [Display(Name = "image url")]
