b3d728cb6e54551a9609a192ce9ca1b720587660
SpamHam <peterthraneolsen@gmail.com>
2014-12-17 18:58:23 +0100

    doc

--- MVCAdminTier/BLLGateway/Gateway/ICityGateway.cs
+++ MVCAdminTier/BLLGateway/Gateway/ICityGateway.cs
@@ -9,6 +9,12 @@ namespace BLLGateway.Gateway
 {
    public interface ICityGateway :IGenericGateway<CityDTO>
     {
+        /// <summary>
+        /// gets a <"City"> by giving a <"zipcode"> to a specific Url <"path">.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="zipcode"></param>
+        /// <returns></returns>
         CityDTO getCityByZipcode(String Path, String zipcode);
     }
 }
