421b33c624fedb7934c2b23513e6be61ba59f8c2
jacobfullbuster <jacob-abildskov@hotmail.com>
2014-12-17 17:11:47 +0100

    yo

--- MVCAdminTier/BLLGateway/DTOModels/AddressDTO.cs
+++ MVCAdminTier/BLLGateway/DTOModels/AddressDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace BLLGateway.DTOModels
    public class AddressDTO : IGenericDTO
     {
         public int id { get; set; }
+       [Display(Name = "Street name")]
         public string streetName { get; set; }
         public string streetNumber { get; set; }
         public int cityId { get; set; }
