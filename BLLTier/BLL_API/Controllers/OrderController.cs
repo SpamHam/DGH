329c63c4c53d2f704afe089be4837db4a447df7e
jacobfullbuster <jacob-abildskov@hotmail.com>
2014-12-17 17:06:55 +0100

    DOC bll v45

--- BLLTier/BLL_API/Controllers/OrderController.cs
+++ BLLTier/BLL_API/Controllers/OrderController.cs
@@ -20,7 +20,10 @@ namespace BLL_API.Controllers
             _facade = new Facade();
         }
 
-
+        /// <summary>
+        /// This method returns an IEnumerable of OrderDTOes from the DAL tier.
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [Route("")]
         public IEnumerable<OrderDTO> GetAll()
@@ -28,7 +31,11 @@ namespace BLL_API.Controllers
             return _facade.GetOrderGateway().GetAll(_url);
         }
 
-
+        /// <summary>
+        /// This method returns a OrderDTO, from the DAL tier, which is speficified by the id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet]
         [Route("{id:int}")]
         public OrderDTO Get(int id)
@@ -36,6 +43,11 @@ namespace BLL_API.Controllers
             return _facade.GetOrderGateway().Get(_url, id);
         }
 
+        /// <summary>
+        /// This method post a OrderDTO, to the DAL tier.
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
         [HttpPost]
         [Route("")]
         public HttpResponseMessage Post(OrderDTO order)
@@ -43,6 +55,11 @@ namespace BLL_API.Controllers
             return _facade.GetOrderGateway().Add(order, _url);
         }
 
+        /// <summary>
+        /// This method put a OrderDTO, to the DAL tier.  
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
         [HttpPut]
         [Route("")]
         public HttpResponseMessage Put(OrderDTO order)
@@ -53,6 +70,11 @@ namespace BLL_API.Controllers
                 _facade.GetProductGateway().GetAll("product")), _url);
         }
 
+        /// <summary>
+        /// This method delete a OrderDTO, to The DAL tier.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete]
         [Route("{id:int}")]
         public HttpResponseMessage Delete(int id)
